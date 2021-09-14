6685d255352795ae3843d9d930d4cdb234b75821
Isabelle Hjelden <hjeld013@falcon.morris.umn.edu>
2021-09-14 10:48:36 -0500

    Finish process_client_log shell file.
    Co-Authored-By: JadynSondrol <sondr037@morris.umn.edu>

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Arguement needed is a directory
 DIRECTORY=$1
 
-cd ../"$DIRECTORY"/var/log
+# Changes directory in to the directory given and then in to /var/log
+cd "$DIRECTORY"/var/log || exit
 
+# cat is used to gatehr the contents of all the log files needed and pipes them to awk
+# awk is used to extract the correct information needed
+# and then prints out the necessary groups
+cat ./* | awk 'match($0, /([a-zA-Z]{3} [ 0-9]{2}) ([0-9]{2})[0-9:]+ [a-zA-Z]+.+ Failed password .+ ([a-zA-Z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4]}' | sed 's/  / /' > failed_login_data.txt
 
-cat * > allLogs.txt
-cat allLogs.txt
-
-#awk 'match($0,/([a-zA-Z] {3}, [0-9] {2}, [0-9] {2}).+ Failed password .+ ([a-zA-z]).+ from .+ ([0-9].)  
-
+#Moves failed_login_data.txt out of two directories
+mv failed_login_data.txt ../../
