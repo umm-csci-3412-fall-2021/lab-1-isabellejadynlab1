f249dbcd986824834be9b3f2afbf7969e7952c98
Isabelle Hjelden <hjeld013@falcon.morris.umn.edu>
2021-09-22 11:05:47 -0500

    Add ability for username to match with _ or -
    Changed the awk match statement in process_client_logs so that the username or name can contain
    '_' or '-'.
    Co-Authored-By= JadynSondrol <sondr037@morris.umn.edu>

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -9,7 +9,7 @@ cd "$DIRECTORY"/var/log || exit
 # cat is used to gather the contents of all the log files needed and pipes them to awk
 # awk is used to extract the correct information needed
 # and then prints out the necessary groups
-cat ./* | awk 'match($0, /([a-zA-Z]{3} [ 0-9]{2}) ([0-9]{2})[0-9:]+ [a-zA-Z]+.+ Failed password .+ ([a-zA-Z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4]}' | sed 's/  / /' > failed_login_data.txt
+cat ./* | awk 'match($0, /([a-zA-Z]{3} [ 0-9]{2}) ([0-9]{2})[0-9:]+ [a-zA-Z]+.+ Failed password .+ ([-/_/a-zA-Z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4]}' | sed 's/  / /' > failed_login_data.txt
 
 #Moves failed_login_data.txt out of two directories
 mv failed_login_data.txt ../../
